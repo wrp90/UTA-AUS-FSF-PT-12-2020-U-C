223fa8c33f2bc04f397c662a04463018d821563d
Will Papp <wpapp90@gmail.com>
2021-02-27 13:59:45 -0600

    notes 2.27.2021

---
+++ 09-NodeJS/01-Activities/22-Stu_for-of/Unsolved/index.js
@@ -0,0 +1,7 @@
+const songs = ['Fast', 'Old Town Road - Remix', 'Wrong', 'Piece Of Your Heart', 'Kill This Love', 'bad guy', 'Verte Ir', 'Beef FloMix', 'Avengers: Endgame', 'Old Town Road'];
+
+const htmlSongs = document.querySelector('#songs')
+
+for (const value of htmlSongs) {
+    element.classList.add("red");
+}
