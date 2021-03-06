8691d610cf15e09fe8645923fbd4be54c3c2fd07
Will Papp <wpapp90@gmail.com>
2021-03-06 13:52:55 -0600

    3.6.2021

--- 10-OOP/01-Activities/24-Stu_Subclasses/Unsolved/car.js
+++ 10-OOP/01-Activities/24-Stu_Subclasses/Unsolved/car.js
@@ -1,7 +1,12 @@
 // TODO: Import the parent class
-
+const vehicle = require('./vehicle')
 // TODO: Create a `Car` class that extends the `Vehicle` class
-
+class car extends vehicle {
+  constructor(id, color, passengers) {
+    super(id, 4, 'beep');
+    this.color
+  }
+}
 const carPassengers = [
   'Aristotle',
   'Confucius',
--- 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Letter.js
+++ 10-OOP/01-Activities/28-Stu_Mini-Project/Develop/lib/Letter.js
@@ -1,5 +1,11 @@
 class Letter {
-  
+  constructor(letter) {
+      this.letter = letter;
+      this.visible = true;
+      if (this.letter === '?') {
+        this.visible = true;
+      }
+  }
 }
 
 module.exports = Letter;
