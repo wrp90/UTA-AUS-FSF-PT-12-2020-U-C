697c57c26544cd910c2d38b519deca1fa4a78725
Will Papp <wpapp90@gmail.com>
2021-04-03 13:56:34 -0500

    4.3

--- 14-MVC/01-Activities/22-Stu_MVC-Review/Unsolved/controllers/homeRoutes.js
+++ 14-MVC/01-Activities/22-Stu_MVC-Review/Unsolved/controllers/homeRoutes.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
+const { User } = require('../models');
 
 router.get('/', async (req, res) => {
   // TODO: Render template with Sequelize data
-  res.render('homepage');
+  try {
+    const sequelizData = await User.findAll ({
+    });
+    res.render(sequelizData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
