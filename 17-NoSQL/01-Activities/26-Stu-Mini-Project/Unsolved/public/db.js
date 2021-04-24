e4891a8075e13a8857a1958233a04371e45f57fc
Will Papp <wpapp90@gmail.com>
2021-04-24 14:05:29 -0500

    notes

--- 17-NoSQL/01-Activities/26-Stu-Mini-Project/Unsolved/public/db.js
+++ 17-NoSQL/01-Activities/26-Stu-Mini-Project/Unsolved/public/db.js
@@ -1,9 +1,18 @@
 let db;
 // create a new db request for a "BudgetDB" database.
+const request = indexedDB.open('BudgetDB', 1);
 
 request.onupgradeneeded = function (event) {
   // create object store called "BudgetStore" and set autoIncrement to true
+  const db = event.target.result;
+
+  const budgetStore = db.createObjectStore('BudgetStore', {
+    keypath: 'itemID',
+    autoIncrement: true
+  });
+  // budgetStore.createIndex('itemIndex', 'item')
 };
+
 
 request.onsuccess = function (event) {
   db = event.target.result;
@@ -15,17 +24,34 @@ request.onsuccess = function (event) {
 
 request.onerror = function (event) {
   // log error here
+  console.log(event.target.errorCode);
 };
 
 function saveRecord(record) {
   // create a transaction on the pending db with readwrite access
   // access your pending object store
   // add record to your store with add method.
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+  const BudgetStore = transaction.objectStore('BudgetStore');
+
+  BudgetStore.add(record);
 }
 
 function checkDatabase() {
+  const transaction = db.transaction(['BudgetStore'], 'readwrite');
+  const BudgetStore = transaction.objectStore('BudgetStore');
   // open a transaction on your pending db
+  const getCursorRequest = BudgetStore.openCursor();
   // access your pending object store
+  getCursorRequest.onsuccess = e => {
+    const cursor = e.target.result;
+    if (cursor) {
+      console.log(cursor.value);
+      cursor.continue();
+    } else {
+      console.log("No documents left!");
+    }
+  };
   // get all records from store and set to a variable
 
   getAll.onsuccess = function () {
@@ -41,8 +67,19 @@ function checkDatabase() {
         .then((response) => response.json())
         .then(() => {
           // if successful, open a transaction on your pending db
+          const transaction = window.BudgetDB.open('BudgetDB', 1);
           // access your pending object store
+          transaction.onsuccess = e => {
+            const request = e.target.resutl;
+            if (request) {
+              console.log(request.value);
+              request.continue();
+            } else {
+              console.log('No items left!');
+            }
+          }
           // clear all items in your store
+          BudgetStore.clear();
         });
     }
   };
