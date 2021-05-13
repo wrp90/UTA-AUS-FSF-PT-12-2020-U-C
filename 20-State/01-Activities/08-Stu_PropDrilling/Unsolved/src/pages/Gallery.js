e442ba295a524fce9fde63e31da65adc14e93d45
Will Papp <wpapp90@gmail.com>
2021-05-12 21:33:06 -0500

    notes

--- 20-State/01-Activities/08-Stu_PropDrilling/Unsolved/src/pages/Gallery.js
+++ 20-State/01-Activities/08-Stu_PropDrilling/Unsolved/src/pages/Gallery.js
@@ -59,7 +59,9 @@ function Gallery() {
       <p className="text-center h3">Click on the arrows to browse users</p>
       <Row>
         {/* Pass props to the card container */}
-        <CardContainer />
+        <CardContainer 
+        
+        />
       </Row>
     </div>
   );
